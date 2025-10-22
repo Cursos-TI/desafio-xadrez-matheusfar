15684a5413208ff00852417577b62c1661293312
matheusfar <matheusf.ar@hotmail.com>
2025-10-22 21:09:24 +0000

    Nivel Novato / Movimentação

--- xadrez.c
+++ xadrez.c
@@ -5,6 +5,46 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
+
+    int t = 0;
+    int b = 0;
+    int r = 0;
+
+    //Mover a torre 5 casas para a direita
+    printf("\n");
+    printf(" ==== MOVIMENTO TORRE ==== \n");
+    printf("\n");
+
+    for (int t = 0; t < 5; t++){    
+        printf("Direita\n"); // Imprime a direção do movimento 
+    }
+
+    //Mover o bisbo 5 casas para para cima é a direita
+    printf("\n");
+    printf(" ==== MOVIMENTO BISPO ==== \n");  
+    printf("\n");
+
+
+    while (b < 5)
+    {
+        printf("Cima, Direita\n"); // Imprime a direção do movimento
+        b++;
+    }
+    
+    //Mover Rainha 8 casas para a esquerda
+    printf("\n");
+    printf(" ==== MOVIMENTO RAINHA ==== \n");
+    printf("\n");
+
+    do {
+
+        printf("Esquerda\n"); // Imprime a direção do movimento
+        r++;
+
+
+    }while(r < 8);
+
+
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
